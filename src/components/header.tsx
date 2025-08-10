0fd87009d05c94d3d9d5b5d9b2593afb48d483ad
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-10 23:11:42 +0000

    this error has been persistenf please fix : createConsoleError@https://6

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -120,11 +120,9 @@ export function Header() {
         <NavigationMenu className="hidden lg:flex">
           <NavigationMenuList>
             <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Home
+                <NavigationMenuLink asChild>
+                    <Link href="/" className={navigationMenuTriggerStyle()}>Home</Link>
                 </NavigationMenuLink>
-              </Link>
             </NavigationMenuItem>
             
             <NavigationMenuItem>
@@ -205,11 +203,9 @@ export function Header() {
             </NavigationMenuItem>
 
              <NavigationMenuItem>
-                <Link href="/contact" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        Contact
-                    </NavigationMenuLink>
-                </Link>
+                <NavigationMenuLink asChild>
+                    <Link href="/contact" className={navigationMenuTriggerStyle()}>Contact</Link>
+                </NavigationMenuLink>
             </NavigationMenuItem>
 
           </NavigationMenuList>
@@ -265,21 +261,19 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link href={props.href!} passHref legacyBehavior>
-          <a
-            ref={ref}
-            className={cn(
-              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-              {children}
-            </p>
-          </a>
-        </Link>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
       </NavigationMenuLink>
     </li>
   );
