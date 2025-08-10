1a147de683775e4086fa3cd577fdba1d3027fdae
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-10 21:28:53 +0000

    TBH ,, i was tryna prototpe this : https://lchafrica.com/hospital#
    you

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,9 +16,6 @@ export default {
       },
     },
     extend: {
-      fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
