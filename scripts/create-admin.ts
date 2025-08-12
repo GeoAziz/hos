fc749698d2a1fc2bf87cd0d19358278d70cdf665
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-12 23:05:00 +0000

    fix : [18:53:30.930] Running build in Washington, D.C., USA (East) – iad

--- scripts/create-admin.ts
+++ scripts/create-admin.ts
@@ -11,23 +11,21 @@ import "dotenv/config";
 let serviceAccount: any;
 
 // Conditionally import the service account key if not in a Vercel environment
-if (!process.env.SERVICE_ACCOUNT_PRIVATE_KEY) {
-  serviceAccount = require('../serviceAccountKey.json');
+if (process.env.SERVICE_ACCOUNT_PRIVATE_KEY) {
+    serviceAccount = {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+        privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    };
+} else {
+    serviceAccount = require('../serviceAccountKey.json');
 }
 
-
-// Correctly type the service account credentials
-const serviceAccountConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || serviceAccount.project_id,
-  clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL || serviceAccount.client_email,
-  // Use environment variable for private key in production, fallback to file for local dev
-  privateKey: (process.env.SERVICE_ACCOUNT_PRIVATE_KEY || serviceAccount.private_key).replace(/\\n/g, '\n'),
-};
 
 // Initialize Firebase Admin SDK if not already initialized
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccountConfig),
+    credential: cert(serviceAccount),
   });
 }
 
