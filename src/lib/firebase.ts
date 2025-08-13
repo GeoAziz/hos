a86908edde96e0f4914f66cf98c3ecc36e7497dd
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-13 08:35:22 +0000

    are we making progress : [04:33:15.522] Running build in Washington, D.C

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,24 @@
 
 
-import { initializeApp, getApps, AppOptions } from 'firebase-admin/app';
+import { initializeApp, getApps, App, AppOptions, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import "dotenv/config";
 
 
+// In a managed environment like Vercel, the Admin SDK needs the project ID
+// to be explicitly passed during initialization at build time.
+const appOptions: AppOptions = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
 // Initialize Firebase Admin SDK only if it hasn't been initialized yet.
 if (!getApps().length) {
-  try {
-    // In a managed environment like Vercel, the Admin SDK needs the project ID
-    // to be explicitly passed during initialization at build time.
-    const appOptions: AppOptions = {
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    };
-    initializeApp(appOptions);
-     console.log("Firebase Admin SDK initialized successfully.");
-  } catch (error: any) {
-    console.error("Firebase Admin SDK initialization error:", error.message);
-  }
+    try {
+        initializeApp(appOptions);
+        console.log("Firebase Admin SDK initialized successfully.");
+    } catch (error: any) {
+        console.error("Firebase Admin SDK initialization error:", error.message);
+    }
 }
 
 const db = getFirestore();
