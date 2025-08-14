329d611ffb13b430bd8a4b4cb29d47ac65bdfc3a
Mohamed Abdinasir <abdinasirm467@gmail.com>
2025-08-14 17:05:01 +0000

    Error: `legacyBehavior` is deprecated and will be removed in a future re

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -193,16 +193,17 @@ export function Header() {
                   {servicesItems.map((item) => (
                     <li key={item.title}>
                       <NavigationMenuLink asChild>
-                        <Link href={item.href} legacyBehavior passHref>
-                          <a className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md">
-                            <div className="text-primary">{item.icon}</div>
-                            <div className="mb-2 mt-4 text-lg font-medium">
-                              {item.title}
-                            </div>
-                            <p className="text-sm leading-tight text-muted-foreground">
-                              {item.description}
-                            </p>
-                          </a>
+                        <Link
+                          href={item.href}
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        >
+                          <div className="text-primary">{item.icon}</div>
+                          <div className="mb-2 mt-4 text-lg font-medium">
+                            {item.title}
+                          </div>
+                          <p className="text-sm leading-tight text-muted-foreground">
+                            {item.description}
+                          </p>
                         </Link>
                       </NavigationMenuLink>
                     </li>
